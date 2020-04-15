aec61463725b41690a5250e107373d927177dfd4
Andrew Dassonville <andrewda@osuosl.org>
2020-04-14 19:14:28 -0700

    Prefix networks with repo name

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
-resource "openstack_networking_network_v2" "openstack_network" {
-    name            = "openstack_network"
+resource "openstack_networking_network_v2" "keepalived_openstack_network" {
+    name            = "keepalived_openstack_network"
     admin_state_up  = "true"
 }
 
-resource "openstack_networking_subnet_v2" "openstack_subnet" {
-    network_id      = "${openstack_networking_network_v2.openstack_network.id}"
+resource "openstack_networking_subnet_v2" "keepalived_openstack_subnet" {
+    network_id      = "${openstack_networking_network_v2.keepalived_openstack_network.id}"
     cidr            = "192.168.60.0/24"
     enable_dhcp     = "false"
     no_gateway      = "true"
 }
 
-resource "openstack_networking_port_v2" "node1_port" {
-    network_id            = "${openstack_networking_network_v2.openstack_network.id}"
+resource "openstack_networking_port_v2" "keepalived_node1_port" {
+    network_id            = "${openstack_networking_network_v2.keepalived_openstack_network.id}"
     admin_state_up        = "true"
     port_security_enabled = "false"
     fixed_ip {
-        subnet_id  = "${openstack_networking_subnet_v2.openstack_subnet.id}"
+        subnet_id  = "${openstack_networking_subnet_v2.keepalived_openstack_subnet.id}"
         ip_address = "192.168.60.11"
     }
 }
 
-resource "openstack_networking_port_v2" "node2_port" {
-    network_id            = "${openstack_networking_network_v2.openstack_network.id}"
+resource "openstack_networking_port_v2" "keepalived_node2_port" {
+    network_id            = "${openstack_networking_network_v2.keepalived_openstack_network.id}"
     admin_state_up        = "true"
     port_security_enabled = "false"
     fixed_ip {
-        subnet_id  = "${openstack_networking_subnet_v2.openstack_subnet.id}"
+        subnet_id  = "${openstack_networking_subnet_v2.keepalived_openstack_subnet.id}"
         ip_address = "192.168.60.12"
     }
 }
@@ -69,7 +69,7 @@ resource "openstack_compute_instance_v2" "node1" {
         uuid = "${data.openstack_networking_network_v2.network.id}"
     }
     network {
-        port = "${openstack_networking_port_v2.node1_port.id}"
+        port = "${openstack_networking_port_v2.keepalived_node1_port.id}"
     }
     provisioner "chef" {
         run_list        = ["recipe[keepalived_test::provisioning]"]
@@ -97,7 +97,7 @@ resource "openstack_compute_instance_v2" "node2" {
         uuid = "${data.openstack_networking_network_v2.network.id}"
     }
     network {
-        port = "${openstack_networking_port_v2.node2_port.id}"
+        port = "${openstack_networking_port_v2.keepalived_node2_port.id}"
     }
     provisioner "chef" {
         run_list        = ["recipe[keepalived_test::provisioning]"]
